3c3a277731d554ea08d59a8eefe1743b5194277e
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-01 12:40:00 -0400

    refactor(app): remove default scaffold and register application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import databaseConfig from "./config/database.config";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService]
+	imports: [
+		ConfigModule.forRoot({
+			load: [databaseConfig]
+		}),
+		TypeOrmModule.forRootAsync(databaseConfig.asProvider()),
+		UsersModule
+	],
+	controllers: [],
+	providers: []
 })
 export class AppModule {}
