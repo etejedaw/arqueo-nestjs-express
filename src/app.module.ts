d524a61c33ecd57c0c8cef223d3c2c8fd0323f11
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-01 13:10:00 -0400

    feat(config): add validated app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import appConfig from "./config/app.config";
 import databaseConfig from "./config/database.config";
 import { UsersModule } from "./users/users.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
-			load: [databaseConfig]
+			load: [appConfig, databaseConfig]
 		}),
 		TypeOrmModule.forRootAsync(databaseConfig.asProvider()),
 		UsersModule
---
+++ src/config/app.config.ts
@@ -0,0 +1,32 @@
+import { registerAs } from "@nestjs/config";
+import { Expose, plainToInstance, Type } from "class-transformer";
+import { IsIn, IsInt, validateSync } from "class-validator";
+
+const NODE_ENVIRONMENTS = ["dev", "prod", "test"] as const;
+type NodeEnvironments = (typeof NODE_ENVIRONMENTS)[number];
+
+class AppConfig {
+	@Expose()
+	@IsIn(NODE_ENVIRONMENTS)
+	NODE_ENV: NodeEnvironments = "dev";
+
+	@Expose()
+	@IsInt()
+	@Type(() => Number)
+	PORT = 3000;
+}
+
+export default registerAs("app", () => {
+	const appConfig = plainToInstance(AppConfig, process.env, {
+		excludeExtraneousValues: true,
+		exposeDefaultValues: true
+	});
+
+	const errors = validateSync(appConfig);
+	if (errors.length > 0) throw new Error(`Invalid app config`);
+
+	return {
+		nodeEnv: appConfig.NODE_ENV,
+		port: appConfig.PORT
+	};
+});
