40b90458648dae2117465c6c5c3ae3784028208a
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-01 13:00:00 -0400

    fix(config): read database values from the environment

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,26 +1,37 @@
 import { registerAs } from "@nestjs/config";
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { plainToInstance, Type } from "class-transformer";
-import { IsInt, IsNotEmpty, IsString, validateSync } from "class-validator";
+import { Expose, plainToInstance, Type } from "class-transformer";
+import {
+	IsInt,
+	IsNotEmpty,
+	IsPositive,
+	IsString,
+	validateSync
+} from "class-validator";
 
 class DatabaseConfig {
+	@Expose()
 	@IsString()
 	@IsNotEmpty()
 	DB_HOST = "localhost";
 
+	@Expose()
 	@IsInt()
-	@IsNotEmpty()
+	@IsPositive()
 	@Type(() => Number)
 	DB_PORT = 5432;
 
+	@Expose()
 	@IsString()
 	@IsNotEmpty()
 	DB_USERNAME = "root";
 
+	@Expose()
 	@IsString()
 	@IsNotEmpty()
 	DB_PASSWORD = "toor";
 
+	@Expose()
 	@IsString()
 	@IsNotEmpty()
 	DB_DATABASE = "arqueo";
@@ -28,7 +39,8 @@ class DatabaseConfig {
 
 export default registerAs("database", (): TypeOrmModuleOptions => {
 	const databaseConfig = plainToInstance(DatabaseConfig, process.env, {
-		excludeExtraneousValues: true
+		excludeExtraneousValues: true,
+		exposeDefaultValues: true
 	});
 
 	const errors = validateSync(databaseConfig);
