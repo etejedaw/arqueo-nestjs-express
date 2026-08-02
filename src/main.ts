2a4771561c4af6587e32e2228cbc8370e117b4aa
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-02 12:10:00 -0400

    feat(bootstrap): apply cors config and enable uri versioning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { ConfigType } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
@@ -6,15 +6,19 @@ import helmet from "helmet";
 
 import { AppModule } from "./app.module";
 import appConfig from "./config/app.config";
+import corsConfig from "./config/cors.config";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+	const cors = app.get<ConfigType<typeof corsConfig>>(corsConfig.KEY);
 
 	app.use(helmet());
 	app.use(cookieParser());
-	app.enableCors();
+	app.enableCors(cors);
+	app.enableVersioning({ type: VersioningType.URI, defaultVersion: "1" });
+	app.enableShutdownHooks();
 
 	app.useGlobalPipes(
 		new ValidationPipe({
@@ -23,7 +27,6 @@ async function bootstrap() {
 			transform: true
 		})
 	);
-	app.enableShutdownHooks();
 
 	await app.listen(config.port);
 }
