4508dcfc02c7485562ce7fa08655681c9702beb9
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-02 12:20:00 -0400

    feat(users): inject repository and implement find one

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,18 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
+	constructor(
+		@InjectRepository(User)
+		private readonly usersRepository: Repository<User>
+	) {}
+
 	create(createUserDto: CreateUserDto) {
 		return `This action adds a new user ${createUserDto.name}`;
 	}
@@ -13,8 +21,14 @@ export class UsersService {
 		return `This action returns all users`;
 	}
 
-	findOne(id: string) {
-		return `This action returns a #${id} user`;
+	async findOne(id: string) {
+		const user = await this.usersRepository.findOneBy({
+			id,
+			isActive: true
+		});
+		if (!user) throw new NotFoundException(`User ${id} not found`);
+
+		return user;
 	}
 
 	update(id: string, updateUserDto: UpdateUserDto) {
