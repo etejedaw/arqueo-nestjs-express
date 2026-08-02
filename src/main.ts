190504bd542a9a51f16b966337b6b58a8a25798b
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-02 11:10:00 -0400

    feat(bootstrap): add global validation pipe and security middleware

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { ValidationPipe } from "@nestjs/common";
 import { ConfigType } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 import { AppModule } from "./app.module";
 import appConfig from "./config/app.config";
@@ -8,6 +11,19 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+
+	app.use(helmet());
+	app.use(cookieParser());
+	app.enableCors();
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true
+		})
+	);
+	app.enableShutdownHooks();
 
 	await app.listen(config.port);
 }
