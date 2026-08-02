557491ff0de394016aa485c2b7d790f2ab55f2ac
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-02 11:20:00 -0400

    feat(app): register global throttler guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import appConfig from "./config/app.config";
@@ -11,10 +13,11 @@ import { UsersModule } from "./users/users.module";
 		ConfigModule.forRoot({
 			load: [appConfig, databaseConfig]
 		}),
+		ThrottlerModule.forRoot([{ ttl: 60_000, limit: 100 }]),
 		TypeOrmModule.forRootAsync(databaseConfig.asProvider()),
 		UsersModule
 	],
 	controllers: [],
-	providers: []
+	providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }]
 })
 export class AppModule {}
