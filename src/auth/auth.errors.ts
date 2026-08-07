f2519b384f27bbd0170e718b33b97cd7a8db95e0
Esteban Tejeda <etejedaw@hotmail.com>
2026-08-06 23:20:00 -0400

    feat(auth): map invalid credentials to unauthorized

--- src/auth/auth.errors.ts
+++ src/auth/auth.errors.ts
@@ -1,6 +1,14 @@
+import { UnauthorizedException } from "@nestjs/common";
+
+import { createErrorsFilter } from "../common/filters/create-errors-filter";
+
 export class InvalidCredentialsError extends Error {
 	constructor() {
 		super("Invalid email or password");
 		this.name = "InvalidCredentialsError";
 	}
 }
+
+export const AuthErrorsFilter = createErrorsFilter([
+	[InvalidCredentialsError, UnauthorizedException]
+]);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,20 @@
 import { Module } from "@nestjs/common";
+import { APP_FILTER } from "@nestjs/core";
 
 import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
+import { AuthErrorsFilter } from "./auth.errors";
 import { AuthService } from "./auth.service";
 import { HashingService } from "./hashing.service";
 
 @Module({
 	imports: [UsersModule],
 	controllers: [AuthController],
-	providers: [AuthService, HashingService],
+	providers: [
+		AuthService,
+		HashingService,
+		{ provide: APP_FILTER, useClass: AuthErrorsFilter }
+	],
 	exports: [AuthService, HashingService]
 })
 export class AuthModule {}
